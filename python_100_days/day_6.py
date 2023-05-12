596a1380d31ded27f08cb330cfd92e1787f97051
Rob <turtlebugdev@gmail.com>
2023-05-12 00:11:28 -0400

    Create day_6.py
    while loops

---
+++ python_100_days/day_6.py
@@ -0,0 +1,46 @@
+##### Simple Functions #####
+# Python Function Syntax
+# def function_name:
+    #Do this
+    #Then do this
+    #Finally do this
+
+
+def hello_user():
+    name = input("What is your name, human? ")
+    print(f"Hello {name}\n You're part of a homemade function now...")
+hello_user()
+
+##### while loop ####
+# While something_is_true:
+    #Do this
+    #Then do this
+    #Then do this
+    #Over and over until something_is_true = something_is_no_longer_true
+
+
+#### Final code for Robot AI day 6 #####
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def move_clear_right():
+#     turn_right()
+#     move()
+    
+# while not is_facing_north():
+#     turn_left()
+# while front_is_clear():
+#         move()
+# turn_left()
+
+# while not at_goal():
+#     if front_is_clear() and wall_on_right():
+#         move()
+#     elif right_is_clear():
+#         move_clear_right()
+#         if right_is_clear():
+#             move_clear_right()
+#     else:
+#         turn_left()  
